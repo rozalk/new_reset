7fda830a623d35e58c3ce02bf07919bca3cdc122
rozalk <rozalkarki9865@gmail.comm>
2024-04-28 19:03:09 +0545

    done

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("Hello I am Rowszal I completed my task");
-console.log("You are good at coding");
+// console.log("Hello I am Rowszal I completed my task");
+// console.log("You are good at coding");
+
+const dob =Number( prompt("Enter your date of birth in year"));
+
+const result = 2024 - dob;
+console.log(`Your age is ${result}`);
+console.log(dob);
